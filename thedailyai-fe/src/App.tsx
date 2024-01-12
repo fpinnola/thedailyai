8f006a265e4860ac31f847d54fe8d2a27694cbdf
Frank Pinnola <frankie4pinnola@gmail.com>
2024-01-11 21:07:08 -0500

    Onboard and save prefs

--- thedailyai-fe/src/App.tsx
+++ thedailyai-fe/src/App.tsx
@@ -1,4 +1,4 @@
-import { redirect, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
--- thedailyai-fe/src/Onboard.tsx
+++ thedailyai-fe/src/Onboard.tsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const newsCategories: string[] = ["economy", "business", "technology", "biotech", "healthcare", "politics", "law", "government", "crime", "weather"];
+const summaryDetailOptions: string[] = ["High-Level Overview", "Moderately Detailed", "Very Detailed"];
+const newsStyleOptions: string[] = ["Newscaster", "Humorous", "Serious", "Relaxed"];
+const PAGES = 3;
+
 
 export default function Onboard() {
     const navigate = useNavigate();
     const [categorySelections, setCategorySelections] = useState<any[]>([]);
+    const [summaryDetail, setSummaryDetail] = useState<string>('');
+    const [selectedNewsStyle, setSelectedNewsStyle] = useState<string>('');
+    const [page, setPage] = useState(0);
 
     useEffect(() => {
         const categories = newsCategories.map((elem) => {
@@ -24,19 +31,81 @@ export default function Onboard() {
         setCategorySelections(newCatetories);
     }
 
-    return (
+    
+
+    const continueBtn = () => {
+        if (page + 1 >= PAGES) {
+            // DONE, navigate to home
+            const preferences = {
+                categories: categorySelections.filter((elem) => elem.selected).map(elem => elem.name),
+                summaryDetail: summaryDetail,
+                style: selectedNewsStyle
+            };
+            localStorage.setItem('preferences', JSON.stringify(preferences));
+            navigate("/home");
+            return;
+        }
+
+        setPage(page + 1);
+    }
+
+    if (page === 0) {
+        return (
+            <>
+                <div>
+                    <h2>What types of news are you interested in?</h2>
+                    <div>
+                        {categorySelections.map(elem => {
+                            return (
+                                <button key={elem.name} className={elem.selected ? "selected" : "unselected"} onClick={() => selectedCategory(elem.name)}>{elem.name}</button>
+                            )
+                        })}
+                    </div>
+                    <button onClick={continueBtn}>Continue</button>
+                </div>
+            </>
+        )
+    } else if (page === 1) {
+        return (
+            <>
+                <div>
+                    <h2>How detailed would you like your news summaries to be?</h2>
+                    <div>
+                        {summaryDetailOptions.map(elem => {
+                            return (
+                                <button key={elem} className={elem === summaryDetail ? "selected" : "unselected"} onClick={() => setSummaryDetail(elem)}>{elem}</button>
+                            )
+                        })}
+                    </div>
+                    <button disabled={!summaryDetail.length} onClick={continueBtn}>Continue</button>
+                </div>
+            </>
+        )
+    } else if (page === 2) {
+        return (
+            <>
+                <div>
+                    <h2>What style would you like your news to be delivered in?</h2>
+                    <div>
+                        {newsStyleOptions.map(elem => {
+                            return (
+                                <button key={elem} className={selectedNewsStyle === elem ? "selected" : "unselected"} onClick={() => setSelectedNewsStyle(elem)}>{elem}</button>
+                            )
+                        })}
+                    </div>
+                    <button disabled={!selectedNewsStyle.length} onClick={continueBtn}>Continue</button>
+                </div>
+            </>
+        )
+    } else {
+        return (
         <>
             <div>
-                <h2>What types of news are you interested in?</h2>
-                <div>
-                    {categorySelections.map(elem => {
-                        return (
-                            <button className={elem.selected ? "selected" : "unselected"} onClick={() => selectedCategory(elem.name)}>{elem.name}</button>
-                        )
-                    })}
-                </div>
-                <button onClick={() => navigate("/home")}>Done</button>
+                <h2>Sorry there is a problem.</h2>
             </div>
         </>
-    )
+        )
+    }
+
+
 }
