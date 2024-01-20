6fb5ba98a851b151ee9e8d0018f82a56f44776b6
Frank Pinnola <frankie4pinnola@gmail.com>
2024-01-19 22:28:43 -0500

    Class abstraction for User db

--- news_be/user_model.py
+++ news_be/user_model.py
@@ -1,40 +1,46 @@
 import datetime
 from pymongo import MongoClient
 
-client = MongoClient("localhost", 27017)
-db = client.news_db
-user_collection = db.user_collection
+class UserModel:
+    def __init__(self) -> None:
+        self.client = MongoClient("localhost", 27017)
+        self.db = self.client.news_db
+        self.users = self.db.user_collection
+        
 
-def get_user(userId):
-    user = user_collection.find_one({'userId': userId})
-    if not user:
-        print(f"User {userId} not found")
-    return user
 
-def save_user_preferences(userId, preferences):
-    new_user = user_collection.update_one({
-        'userId': userId
-    }, {
-        '$set': { 'preferences': preferences }
-    }, upsert=True)
+    def get_user(self, userId):
+        user = self.users.find_one({'userId': userId})
+        if not user:
+            print(f"User {userId} not found")
+        return user
 
-    return new_user
+    def save_user_preferences(self, userId, preferences):
+        new_user = self.users.update_one({
+            'userId': userId
+        }, {
+            '$set': { 'preferences': preferences }
+        }, upsert=True)
 
-# TODO: implement
-def update_user_articles(userId, articles):
-    pass
+        return new_user
 
-# TODO: implement
-def get_user_articles(userId):
-    pass
+    # TODO: implement
+    def update_user_articles(self, userId, articles):
+        pass
+
+    # TODO: implement
+    def get_user_articles(self, userId):
+        pass
 
 if __name__ == '__main__':
-    print(get_user('abc123'))
+    users = UserModel()
+
+    print(users.get_user('abc123'))
 
     preferences = {
         'categories': ['biotech', 'tech']
     }
 
-    save_user_preferences('abc123', preferences)
+    users.save_user_preferences('abc123', preferences)
 
-    print(get_user('abc123'))
+    print(users.get_user('abc123'))
